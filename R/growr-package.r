0d48f6e8b28a2ccb3fdb7ca4e05a79072e5d0c43
aaronwolen <aaron@wolen.com>
2015-02-04 09:12:48 -0500

    Add growr-package.r

---
+++ R/growr-package.r
@@ -0,0 +1,12 @@
+#' WHO growth charts
+#'
+#' Generate growth charts based on WHO data.
+#'
+#' @author
+#' Aaron Wolen \email{aaron@@wolen.com}
+#'
+#' Maintainer: Aaron Wolen \email{aaron@@wolen.com}
+#' @name growr
+#' @docType package
+#' @import ggvis
+NULL
