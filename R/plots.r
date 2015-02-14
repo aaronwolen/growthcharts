8a0ab83d52e97b7cca62f2f4d28309b6e54599a3
aaronwolen <aaron@wolen.com>
2015-02-14 12:19:39 -0500

    Add weight_for_length()

--- R/plots.r
+++ R/plots.r
@@ -13,3 +13,20 @@ circumference_for_age <- function(circumference, age, gender = "male", unit.leng
     ggvis(~month, ~circumference, stroke = ~percentile) %>%
     layer_lines()
 }
+
+
+#' Head circumference for age
+
+weight_for_length <- function(weight, length, gender = "male", unit.length = "lb") {
+
+  gender <- check_gender(gender)
+
+  data <- .table$weight.length %>%
+    filter_(lazyeval::interp("gender == x", x = gender)) %>%
+    select(-gender)
+
+  data %>%
+    group_by(percentile) %>%
+    ggvis(~length, ~weight, stroke = ~percentile) %>%
+    layer_lines()
+}
