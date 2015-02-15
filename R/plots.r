9953e15cd5de1094acd94c13d71b258aa0643096
aaronwolen <aaron@wolen.com>
2015-02-15 10:30:09 -0500

    plots: Implement remaining plot types

--- R/plots.r
+++ R/plots.r
@@ -1,12 +1,26 @@
 #' Head circumference for age
-
+#' @export
 circumference_for_age <- function(circumference = NULL, age = NULL, gender = "male", unit.length = "cm") {
   build_plot(circumference, age, gender, unit.length)
 }
 
 
 #' Weight for length
-
+#' @export
 weight_for_length <- function(weight = NULL, length = NULL, gender = "male", unit.length = "kg") {
   build_plot(weight, length, gender, unit.length)
+}
+
+
+#' Length for age
+#' @export
+length_for_age <- function(length = NULL, age = NULL, gender = "male", unit.length = "cm") {
+  build_plot(length, age, gender, unit.length)
+}
+
+
+#' Weight for age
+#' @export
+weight_for_age <- function(weight = NULL, age = NULL, gender = "male", unit.length = "kg") {
+  build_plot(weight, age, gender, unit.length)
 }
