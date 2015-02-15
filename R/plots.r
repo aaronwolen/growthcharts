453c2eeb873123f5db26fc5d42addbb433cfecd9
aaronwolen <aaron@wolen.com>
2015-02-14 20:16:54 -0500

    Consolidate plot functions

--- R/plots.r
+++ R/plots.r
@@ -1,32 +1,35 @@
 #' Head circumference for age
 
-circumference_for_age <- function(circumference, age, gender = "male", unit.length = "in") {
-
-  gender <- check_gender(gender)
-
-  data <- .table$circumference.age %>%
-            filter_(lazyeval::interp("gender == x", x = gender)) %>%
-            select(-gender)
-
-  data %>%
-    group_by(percentile) %>%
-    ggvis(~month, ~circumference, stroke = ~percentile) %>%
-    layer_lines()
+circumference_for_age <- function(circumference = NULL, age = NULL, gender = "male", unit.length = "in") {
+  build_plot(circumference, age, gender, unit.length)
 }
 
 
 #' Head circumference for age
 
-weight_for_length <- function(weight, length, gender = "male", unit.length = "lb") {
+weight_for_length <- function(weight = NULL, length = NULL, gender = "male", unit.length = "lb") {
+  build_plot(weight, length, gender, unit.length)
+}
 
-  gender <- check_gender(gender)
+build_plot <- function(...) {
 
-  data <- .table$weight.length %>%
-    filter_(lazyeval::interp("gender == x", x = gender)) %>%
-    select(-gender)
+  # parse arguments
+  args <- structure(list(...), names = as.character(match.call()[-1]))
+  args$gender <- check_gender(args$gender)
 
-  data %>%
-    group_by(percentile) %>%
-    ggvis(~length, ~weight, stroke = ~percentile) %>%
-    layer_lines()
+  # find corresponding data table
+  tbl.code <- paste(names(args)[1:2], collapse = ".")
+  df <- .table[[tbl.code]]
+  if (is.null(df)) stop(tbl.code, " data is not available.", call. = FALSE)
+
+  df <- filter(df, gender == args$gender) %>% select(-gender)
+
+  # rename category to matching variables
+  xy <- names(args)[1:2]
+  xy <- c(age = "month", circumference = "circumference")[xy]
+
+  ggvis(df) %>%
+   group_by(percentile) %>%
+   layer_points(prop("x", as.name(xy[1])),
+                prop("y", as.name(xy[2])))
 }
