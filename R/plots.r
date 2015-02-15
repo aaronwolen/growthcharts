1399f8b71e2eefcebbb1521db02b8cf0a86bb539
aaronwolen <aaron@wolen.com>
2015-02-15 14:05:23 -0500

    Document parameters

--- R/plots.r
+++ R/plots.r
@@ -1,4 +1,10 @@
 #' Head circumference for age
+#'
+#' @param circumference head circumference
+#' @param age age in months at the time of trait measurment
+#' @param gender (\code{male} or \code{female})
+#' @param units character vector indicating the units of measures used for the
+#'   plotted traits
 #' @export
 circumference_for_age <- function(circumference = NULL, age = NULL, gender = "male", units = c("cm", "month")) {
   build_plot(circumference, age, gender, units)
@@ -6,6 +12,9 @@ circumference_for_age <- function(circumference = NULL, age = NULL, gender = "ma
 
 
 #' Weight for length
+#' @param weight child's weight
+#' @param length child's length
+#' @inheritParams circumference_for_age
 #' @export
 weight_for_length <- function(weight = NULL, length = NULL, gender = "male", units = c("kg", "cm")) {
   build_plot(weight, length, gender, units)
@@ -13,6 +22,8 @@ weight_for_length <- function(weight = NULL, length = NULL, gender = "male", uni
 
 
 #' Length for age
+#' @inheritParams weight_for_length
+#' @inheritParams circumference_for_age
 #' @export
 length_for_age <- function(length = NULL, age = NULL, gender = "male", units = c("cm", "month")) {
   build_plot(length, age, gender, units)
@@ -20,6 +31,8 @@ length_for_age <- function(length = NULL, age = NULL, gender = "male", units = c
 
 
 #' Weight for age
+#' @inheritParams weight_for_length
+#' @inheritParams circumference_for_age
 #' @export
 weight_for_age <- function(weight = NULL, age = NULL, gender = "male", units = c("kg", "month")) {
   build_plot(weight, age, gender, units)
