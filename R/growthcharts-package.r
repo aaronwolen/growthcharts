e743dc6f7458a0fdc4d6a84d08d601b8ba810caa
aaronwolen <aaron@wolen.com>
2015-02-28 14:46:48 -0500

    Add description of data

--- R/growthcharts-package.r
+++ R/growthcharts-package.r
@@ -1,6 +1,7 @@
-#' WHO growth charts
+#' Growth charts
 #'
-#' Generate growth charts based on WHO data.
+#' Growth charts for infants and children ages 0 to 2 using the World Health
+#' Organization's child growth standards.
 #'
 #' @author
 #' Aaron Wolen \email{aaron@@wolen.com}
@@ -13,4 +14,17 @@
 #' @importFrom ggvis ggvis layer_paths layer_points layer_text prop add_axis
 #' @importFrom scales brewer_pal
 #' @importFrom lazyeval interp
+NULL
+
+
+#' WHO Growth Standards
+#'
+#' Growth charts are generated using summary statistics calculated using the
+#' World Health Organization's Child Growth Standards and made available by the
+#' Centers for Disease Control.
+#'
+#' @references
+#' WHO Multicentre Growth Reference Study Group. \href{http://www.who.int/entity/childgrowth/standards/Technical_report.pdf}{WHO Child Growth Standards: Length/height-for-age, weight-for-age, weight-for-length, weight-for-height and body mass index-for-age: Methods and development}. Geneva: World Health Organization, 2006
+#' @docType data
+#' @name growthdata
 NULL
